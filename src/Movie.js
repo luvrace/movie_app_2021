3cb165cda90e57294c999668560733140558ead8
luvrace <rambn1993@knou.ac.kr>
2021-02-15 22:49:43 +0900

    Add movies css

--- src/Movie.js
+++ src/Movie.js
@@ -1,10 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
+import './Movie.css';
 
 //component가 state가 필요하지 않아 function component로 작성한다
 
 function Movie({id, year, title, summary, poster}){
-    return <h1>{title}</h1>;
+    return (
+        <div class="movies">
+            <img src={poster} alt={title} title={title}/>
+            <div class="movie__data">
+                <h3 class="movie__title">{title}</h3>
+                <h5 class="movie__year">{year}</h5>
+                <p class="movie__summary">{summary}</p>
+            </div>
+        </div>
+    )
 }
 
 Movie.propTypes = {
