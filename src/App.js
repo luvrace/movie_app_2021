3192e11a623fd058c6bd03e3dce7ed91bc22b6e2
luvrace <rambn1993@knou.ac.kr>
2021-02-16 21:46:47 +0900

    FIX css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import axios from "axios";
 import Movie from "./Movie";
-import "./App.css";
+import "./Movie.css";
 
 class App extends React.Component {
     state = {
@@ -28,21 +28,21 @@ class App extends React.Component {
     render(){
         const {isLoading, movies} = this.state;
         return (
-         <section class="container">
+         <section className="container">
             {isLoading ?(
-                <div class="loader">
-                    <span class="loader__text">Loading...</span>
+                <div className="loader">
+                    <span className="loader__text">Loading...</span>
                 </div>
              ):(
-                <div class="movies">
+                <div className="movies">
                 {movies.map(movie => (
                  //map에는 무조건 return이 필요하다.
                  <Movie 
-                    id={movie.id} 
                     year={movie.year} 
                     title={movie.title} 
                     summary={movie.summary} 
                     poster={movie.medium_cover_image}
+                    genres={movie.genres}
                 />
                 ))}
                 </div>
