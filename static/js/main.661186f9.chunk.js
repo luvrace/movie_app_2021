85c1b07bb8fef8d5cb00d4dc438d8cefaec5480b
luvrace <rambn1993@knou.ac.kr>
2021-02-17 20:07:47 +0900

    Updates

--- static/js/main.661186f9.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpmovie_app=this.webpackJsonpmovie_app||[]).push([[0],{435:function(e,t,n){"use strict";n.r(t);n(1);var o=n(20),a=n.n(o),c=(n(134),n(129)),s=n(3),i=n(30);var p=function(){return console.log("about"),Object(i.jsx)("span",{children:"About this page : I built in because I love movies"})};var r=function(){return Object(i.jsx)(c.a,{children:Object(i.jsx)(s.a,{path:"/about",component:p})})};a.a.render(Object(i.jsx)(r,{}),document.getElementById("potato"))}},[[435,1,2]]]);
-//# sourceMappingURL=main.661186f9.chunk.js.map
